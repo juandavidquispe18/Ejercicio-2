26072bb3cdeb3fc4cd493759f5126cc2e8323e98
USUARIO <USUARIO@DESKTOP-N0DKPVM>
2023-09-20 00:41:23 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar y deefinir datos
+            Console.Write("Ingrese la base: ");
+            float base_01 = float.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el exponente: ");
+            int potencia = int.Parse(Console.ReadLine());
+
+            double resultado = 1;
+            
+            //Hallar la potencia
+            for (int i = 0; i < potencia; i++) {
+                resultado *= base_01;
+            }
+
+            //Mostrar resultado
+            Console.WriteLine($"La potencia es: {resultado}");
+            Console.ReadKey();
+        }
+    }
+}
